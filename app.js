25f1d2f4c7426c7a4644bdc3815d7db6954eab54
Brandon Gonzalez <brajgon@gmail.com>
2024-04-07 12:08:18 -0600

    second commit

--- app.js
+++ app.js
@@ -1,32 +1,50 @@
 // DOM Elements
 const studentForm = document.getElementById("studentForm");
-const studentsContainer = document.getElementById(".students");
+const studentsContainer = document.querySelector(".students");
 const nameInput = studentForm['name'];
 const ageInput = studentForm['age'];
 const rollInput = studentForm['roll'];
 
 
-/* 
-{
-  name: '',
-  age: number,
-  roll: number
-}
-*/
-
-const students = [{
-    name: "Brandon",
-    age: 30,
-    roll: 1
-}];
+// const students = [{
+//     name: "Brandon",
+//     age: 30,
+//     roll: 1
+// }];
 
 const addStudent = (name, age, roll) => {
-    
+    students.push({
+        name,
+        age,
+        roll
+    });
+    return { name, age, roll };
 }
 
-const createStudentElement = () => {
+const createStudentElement = ({ name, age, roll }) => {
+    // create elements
     const studentDiv = document.createElement('div');
     const studentName = document.createElement('h2');
-    const studentAge = document.createElement('p')
-    const studentRoll = document.createElement('p')
-}
+    const studentAge = document.createElement('p');
+    const studentRoll = document.createElement('p');
+    // fill the content 
+    studentName.innerText = "Student Name: " + name;
+    studentAge.innerText = "Student Age: " + age;
+    studentRoll.innerText = "Student Roll: " + roll;
+    // add to the DOM
+    studentDiv.append(studentName, studentAge, studentRoll);
+    studentsContainer.appendChild(studentDiv)
+};
+
+students.forEach(createStudentElement);
+studentForm.onsubmit = (e) => {
+    e.preventDefault();
+
+    const newStudent = addStudent(
+        nameInput.value,
+        ageInput.value,
+        rollInput.value
+    );
+
+    createStudentElement(newStudent)
+};
